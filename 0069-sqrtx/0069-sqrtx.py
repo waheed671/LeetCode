1be1e47848b687c034297550dc943c69d586b0f0
waheed671 <24705a0244@aitsrajampet.ac.in>
2025-11-14 22:54:19 +0530

    Time: 7 ms (28.51%), Space: 12.5 MB (50.82%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x  # 0 or 1
+
+        left, right = 1, x // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right  # right will be the integer part of sqrt(x)
+
+        
