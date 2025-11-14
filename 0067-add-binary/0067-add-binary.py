05a13e05856099149eae2bc863b33d7023e3290a
waheed671 <24705a0244@aitsrajampet.ac.in>
2025-11-14 22:53:52 +0530

    Time: 5 ms (41.46%), Space: 12.4 MB (85.19%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        result = []
+
+        # Loop from the end of both strings
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            
+            # Append current bit and update carry
+            result.append(str(total % 2))
+            carry = total // 2
+
+        # Reverse and join the result
+        return ''.join(result[::-1])
