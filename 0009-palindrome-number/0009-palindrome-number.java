bddb2d33186d4d5db91b89976832c1f10e5933c2
waheed671 <24705a0244@aitsrajampet.ac.in>
2025-12-16 11:17:10 +0530

    Time: 5 ms (73.82%), Space: 45.9 MB (50.12%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+        while(x>0){
+            int ld=x%10;
+            rev=(rev*10)+ld;
+            x=x/10;
+        }
+        if(rev==temp){
+            return true;
+        }
+        else{  
+            return false;
+        }
+    }
+}
